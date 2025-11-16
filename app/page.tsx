db80fd93f336f382b310ad43ed2b2c3c37ce4c53
asifmollik93 <asifmollik93@gmail.com>
2025-11-16 05:47:18 +0000

    feat: dx

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ import SignUp from "@/components/sign-up";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-background flex flex-col gap-4 p-4">
-      {/* <h1>Hello World</h1> */}
-      <SignUp />
+    <div className="min-h-screen w-full bg-background flex flex-col gap-4 p-4 items-center justify-center">
       <SignIn />
+      <SignUp />
     </div>
   );
 }
